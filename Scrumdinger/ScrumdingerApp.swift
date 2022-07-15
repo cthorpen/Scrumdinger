ed011bf3443fb0fbea105782a4b0ee82bc68273c
Cole Thorpen <thorpencole@gmail.com>
2022-07-15 14:16:49 -0700

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Cole Thorpen on 7/15/22.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
